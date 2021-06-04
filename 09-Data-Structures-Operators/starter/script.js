6591416c72aed5b33e39f86f68e1b2252d5b78ba
Tânia P <taniapestana@gmail.com>
2021-06-04 19:02:32 +0100

    camel case conversion

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -188,7 +188,7 @@ checkBaggage('Got some snacks and a gun for protection')
 
  */
 
-
+/* 
 console.log('a+very+nice+string'.split('+'));
 console.log('Jonas Schmedtmann'.split(' '));
 
@@ -245,6 +245,20 @@ const planesInLine = function(n){
 
 planesInLine(5);
 planesInLine(10);
-planesInLine(20)
+planesInLine(20) */
 
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
 
+document.querySelector('button').addEventListener
+('click', function(){
+const text = document.querySelector('textarea').value;
+const rows = text.split('\n');
+
+for(const row of rows){
+const [first, second] = row.toLowerCase().trim().split('_');
+//console.log(row, first, second);
+const output = `${first}${second.replace(second[0], second[0].toUpperCase())}`;
+console.log(output);
+}
+});
