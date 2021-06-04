40e426925fee391e7cbe6756031579971a0bf4b5
Tânia P <taniapestana@gmail.com>
2021-06-04 17:26:24 +0100

    strings

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -118,7 +118,7 @@ for(const [min,event]of gameEvents){
 
 
 
-const airline = `TAP Air Portugal`;
+/* const airline = `TAP Air Portugal`;
 
 console.log(airline.toLowerCase());
 console.log(airline.toUpperCase());
@@ -185,3 +185,66 @@ if(baggage.includes('Knife')||baggage.includes('gun')){
 checkBaggage('I have a laptop, some Food and a pocket Knife');
 checkBaggage('Socks and camera');
 checkBaggage('Got some snacks and a gun for protection')
+
+ */
+
+
+console.log('a+very+nice+string'.split('+'));
+console.log('Jonas Schmedtmann'.split(' '));
+
+const [firstName, lastName] = 'Jonas Schmedtmann'.split(' ');
+
+const newName = ['Mr.', firstName, lastName.toUpperCase()].join(' ');
+console.log(newName)
+
+const capitalizeName = function(name){
+  const names = name.split(' ');
+  const namesUpper = [];
+  for(const n of names){
+namesUpper.push(n[0].toUpperCase() + n.slice(1))
+  }
+console.log(namesUpper.join(' '))
+}
+
+
+capitalizeName('jessica ann smith davis') 
+capitalizeName('Jonas schmedtmann') 
+
+
+
+
+///Padding 
+
+const message = 'Go to gate 23!'
+console.log(message.padStart(25,'+').padEnd(38, '🌕'));
+
+console.log('Jonas'.padStart(25,'⭐️').padEnd(30, '⭐️'));
+
+
+const maskCreditCard = function(number){
+  const str = number + '';
+  const last = str.slice(-4);
+  return last.padStart(str.length, '*');
+}
+
+console.log(maskCreditCard(883838383));
+console.log(maskCreditCard(68845383845343383));
+console.log(maskCreditCard('1231884538384534123123383'));
+
+
+
+//Repeat
+
+const message2 = 'Bad weather...All Departuers Delayed...\n'
+
+console.log(message2.repeat(5))
+
+const planesInLine = function(n){
+  console.log(`There are ${n} planes in line ${'🛩'.repeat(n)}`)
+}
+
+planesInLine(5);
+planesInLine(10);
+planesInLine(20)
+
+
